357231d7e598d5ce87aa405f5f47ca73de6cbe50
Filippo Squillace <squillace.filippo@gmail.com>
2020-01-31 19:41:24 +0100

    Update integ test

--- tests/integ-tests/integ-tests.sh
+++ tests/integ-tests/integ-tests.sh
@@ -4,16 +4,16 @@ set -e
 
 source $HOME/.bashrc
 
-echo -e "\n\n" | pearl install test
+echo -e "\n\n" | pearl --verbose install test
 
-[ -d $PEARL_HOME/packages/default/test ] || { echo "Error: The package test does not exist after installing it."; exit 1; }
+[ -d $PEARL_HOME/packages/local/test ] || { echo "Error: The package test does not exist after installing it."; exit 1; }
 
 cat $HOME/.gtkrc-2.0
 cat $HOME/.config/gtk-3.0/settings.ini
 
-echo -e "\n\n" | pearl update test
+echo -e "\n\n" | pearl --verbose update test
 
-pearl remove test
+pearl --no-confirm --verbose remove test
 
 cat $HOME/.gtkrc-2.0
 ls -l $HOME/.config/gtk-3.0
